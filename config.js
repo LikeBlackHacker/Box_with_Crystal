5ca0b18978932e2ce62c1cf129c071cf85b93dde
Mital <51521843+LikeBlackHacker@users.noreply.github.com>
2025-03-23 00:20:33 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi em",
+
+    background: "background.jpg",
+
+    min1: "min1.jpeg",
+    min2: "min2.jpeg",
+    min3: "min3.jpeg",
+    min4: "min4.jpg",
+    min5: "min5.jpeg",
+    min6: "min6.jpg",
+
+    max1: "max1.jpg",
+    max2: "max2.jpg",
+    max3: "max3.jpeg",
+    max4: "max4.jpeg",
+    max5: "max5.jpg",
+    max6: "max6.jpg", 
+}
